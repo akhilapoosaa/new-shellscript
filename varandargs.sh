0012fe121ed879672c988bfcb68b12be8319257e
akhilapoosaa <akhila.1829@gmail.com>
2025-03-12 13:15:29 +0530

    args updated

---
+++ varandargs.sh
@@ -0,0 +1,11 @@
+#!bin/bash
+
+#passing arguments to the script
+
+person1=$1
+person2=$2
+
+echo "$person1: hello $person2, how are you?"
+echo "$person2: i am good $person1, how about you?"
+echo "$person1: i am also good $person2, thanks for asking"
+echo "$person2: you are welcome $person1"
