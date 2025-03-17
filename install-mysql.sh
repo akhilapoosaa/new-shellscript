24046c152a47615d0ac73c1dc9b70a68bf9a6233
akhilapoosaa <akhila.1829@gmail.com>
2025-03-17 13:28:54 +0530

    sqlserver  updated

--- install-mysql.sh
+++ install-mysql.sh
@@ -7,6 +7,7 @@ ID=$(id -u)  #get the user id by using the id command
 if [ $ID -ne 0 ] #if the user id is not equal to 0 then the user is not a root user
 then
     echo "ERROR:: you are not the root user, please switch to root user"
+    exit 1 #exit the script if it is thrown an error
 else
     echo "you are the root user"    
 fi
