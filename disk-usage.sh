2b32077d448d0ef186b5c7a608d6cd84e4faf7e8
akhilapoosaa <akhila.1829@gmail.com>
2025-05-12 14:08:26 +0530

    diskspace updated

--- disk-usage.sh
+++ disk-usage.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-DISK_USAGE=$(df -hT | grep -vE 'tmp|File|')
+DISK_USAGE=$(df -hT | grep -vE 'tmp|File')
 DISK_THRESHOLD=1
+message=""  
+#message variable to store the output
 # -h option is used to display the disk usage in human-readable format
 # -T option is used to display the file system type
 # grep -vE option is used to avoid the lines that contain the string 'tmp' or 'File'
 # -v option is used to invert the match
 # -E option is used to enable extended regular expressions
-
-message=""  #message variable to store the output
 
 while IFS= read line
 do 
