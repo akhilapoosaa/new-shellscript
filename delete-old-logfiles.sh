31a13d53719d4902a81df978fa5dc9d1b048671c
akhilapoosaa <akhila.1829@gmail.com>
2025-05-12 13:05:35 +0530

    logs updated

---
+++ delete-old-logfiles.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+#delete old log files
+# This script is used to delete old log files from the /tmp directory.
+# It deletes all log files that are older than 7 days.
+
+SOURCE_DIR=/tmp/shellscript-logs
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+#! is used to check if the directory does not exist
+# -d option is used to check if the directory exists
+if [ ! -d $SOURCE_DIR ] 
+then
+    echo -e "$R ERROR:: $SOURCE_DIR directory does not exist $N"
+    exit 1
+else
+    echo -e "$G SUCCESS:: $SOURCE_DIR directory exists $N"
+fi
+
+FILES_TO_DELETE=$(find $SOURCE_DIR -type f -name "*.log" -mtime +14)
+# -type f option is used to find files
+# -name option is used to find files with the name *.log
+# -mtime option is used to find files that are older than 7 days
